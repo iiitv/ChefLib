140141a303e47ec20e20c6d9e8dffc344caa4351
Dipansh Khandelwal <deepanshrocks438@gmail.com>
2017-09-03 14:59:22 +0530

    Update CHEFSIGN.py

--- 2017/JULY/JULY17/CHEFSIGN/CHEFSIGN.py
+++ 2017/JULY/JULY17/CHEFSIGN/CHEFSIGN.py
@@ -1,18 +1,22 @@
-//made by DipanshKhandelwal
+#made by DipanshKhandelwal
+#this is a python3 file
 if __name__ == '__main__':
-	N = int(input())
-	for _ in range(N):
-		maxfreq=0
-		presentfreq=0
-		present = '='
-		string = input()
-		for i in string	:
-			if i == '>' or i == '<':
-				if i == present :
-					presentfreq +=1
-				else:
-					present = i
-					presentfreq = 1
-			if presentfreq>maxfreq:
-						maxfreq = presentfreq
-		print (maxfreq+1)
+	def main():
+		N = int(input())
+		for _ in range(N):
+			maxfreq=0
+			presentfreq=0
+			present = '='
+			string = input()
+			for i in string	:
+				if i == '>' or i == '<':
+					if i == present :
+						presentfreq +=1
+					else:
+						present = i
+						presentfreq = 1
+				if presentfreq>maxfreq:
+							maxfreq = presentfreq
+			print (maxfreq+1)
+
+main()
