1337cbe8514adfa208d870474e0991d9965ae8b1
DipanshKhandelwal <deepanshrocks438@gmail.com>
2017-09-03 12:30:23 +0530

    Added NITIKA (JULY17) [Python] Fixes #72

---
+++ 2017/JULY/JULY17/NITIKA/NITIKA.py
@@ -0,0 +1,17 @@
+//made by DipanshKhandelwal
+if __name__ == '__main__':
+	N = int(input())
+	for _ in range(N):
+		string = input()
+		my_list = string.split()
+		x= True
+		for i in range(len(my_list)-1,-1,-1):
+			if x:
+				temp = my_list[i].lower()
+				temp2 = my_list[i][0].upper()
+				temp = temp[1:]
+				final = temp2 + temp
+				x = False
+			else :
+				final =  my_list[i][0].upper()+". "+final
+		print (final)
