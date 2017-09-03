e64a80be3a1fb8c9b4fab952b55d3dc0d584c893
Dipansh Khandelwal <deepanshrocks438@gmail.com>
2017-09-03 15:40:26 +0530

    Update NITIKA.py

--- 2017/JULY/JULY17/NITIKA/NITIKA.py
+++ 2017/JULY/JULY17/NITIKA/NITIKA.py
@@ -1,21 +1,21 @@
 #made by DipanshKhandelwal
 #this is a python3 file
+def main():
+	N = int(input())
+	for _ in range(N):
+		string = input()
+		my_list = string.split()
+		x= True
+		for i in range(len(my_list)-1,-1,-1):
+			if x:
+				temp = my_list[i].lower()
+				temp2 = my_list[i][0].upper()
+				temp = temp[1:]
+				final = temp2 + temp
+				x = False
+			else :
+				final =  my_list[i][0].upper()+". "+final
+		print (final)
+            
 if __name__ == '__main__':
-	def main():
-		N = int(input())
-		for _ in range(N):
-			string = input()
-			my_list = string.split()
-			x= True
-			for i in range(len(my_list)-1,-1,-1):
-				if x:
-					temp = my_list[i].lower()
-					temp2 = my_list[i][0].upper()
-					temp = temp[1:]
-					final = temp2 + temp
-					x = False
-				else :
-					final =  my_list[i][0].upper()+". "+final
-			print (final)
-
-main()
+    main()
