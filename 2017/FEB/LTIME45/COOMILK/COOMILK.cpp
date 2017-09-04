8d6a44fc20e03e0d6249ca83940147d6ec125c9e
PiyushPawar17 <piyushpawar25@gmail.com>
2017-09-04 19:42:20 +0530

    Added COOMILK (LTIME45) [C++]

---
+++ 2017/FEB/LTIME45/COOMILK/COOMILK.cpp
@@ -0,0 +1,39 @@
+// Code by Piyush
+
+#include <bits/stdc++.h>
+#define endl "\n"
+using namespace std;
+
+int main() {
+
+    std::ios::sync_with_stdio(false);
+
+    int testCases;
+    cin >> testCases;
+
+    while(testCases--) {
+
+        int minutes;
+        cin >> minutes;
+
+        string arr[minutes];
+
+        for (int i = 0; i < minutes; i++)
+            cin >> arr[i];
+
+        bool instructions = true;
+
+        for (int i = 0; i < minutes; i++) {
+            if (arr[i] == "cookie" && arr[i + 1] != "milk") {
+                instructions = false;
+                break;
+            }
+        }
+
+        if (instructions) cout << "YES" << endl;
+        else cout << "NO" << endl;
+
+    }
+
+    return 0;
+}
