225a5e66da3301acdc9c1e42bb3f082658587661
PiyushPawar17 <piyushpawar25@gmail.com>
2017-09-05 15:09:15 +0530

    Removed blank lines

--- 2017/FEB/LTIME45/COOMILK/COOMILK.cpp
+++ 2017/FEB/LTIME45/COOMILK/COOMILK.cpp
@@ -3,37 +3,28 @@
 #include <bits/stdc++.h>
 #define endl "\n"
 using namespace std;
-
 int main() {
 
     std::ios::sync_with_stdio(false);
-
     int testCases;
     cin >> testCases;
-
     while(testCases--) {
-
         int minutes;
         cin >> minutes;
-
         string arr[minutes];
-
         for (int i = 0; i < minutes; i++)
             cin >> arr[i];
-
         bool instructions = true;
-
         for (int i = 0; i < minutes; i++) {
             if (arr[i] == "cookie" && arr[i + 1] != "milk") {
                 instructions = false;
                 break;
             }
         }
-
-        if (instructions) cout << "YES" << endl;
-        else cout << "NO" << endl;
-
+        if (instructions)
+          cout << "YES" << endl;
+        else
+          cout << "NO" << endl;
     }
-
     return 0;
 }
