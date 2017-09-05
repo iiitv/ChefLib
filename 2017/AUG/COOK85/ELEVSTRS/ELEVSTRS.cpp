1330820a8964a6f6b8558d00d2a4247fe16022a9
Piyush Pawar <piyushpawar25@gmail.com>
2017-09-05 16:27:57 +0530

    ELEVSTRS (COOK85) [C++] (#89)
    * Added ELEVSTRS (COOK85) [C++]
    
    * Updated Readme
    
    * Added Copyright Comment
    
    * Removed blank lines

---
+++ 2017/AUG/COOK85/ELEVSTRS/ELEVSTRS.cpp
@@ -0,0 +1,20 @@
+// Code by Piyush
+
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+
+    std::ios::sync_with_stdio(false);
+    int testCases;
+    cin >> testCases;
+    double sr2 = sqrt(2);
+    while(testCases--) {
+        double n, v1, v2;
+        cin >> n >> v1 >> v2;
+        if((sr2 * n / v1) <= (2 * n / v2))
+          cout << "Stairs\n";
+        else
+          cout << "Elevator\n";
+    }
+    return 0;
+}
