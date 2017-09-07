c96b7ed0bf09991613424f6da64ecf122d4943e2
PiyushPawar17 <piyushpawar25@gmail.com>
2017-09-07 15:38:49 +0530

    LISDIGIT (COOK78) [C++]

---
+++ 2017/JAN/COOK78/LISDIGIT/LISDIGIT.cpp
@@ -0,0 +1,22 @@
+// Code by Piyush
+#include <bits/stdc++.h>
+#define endl "\n"
+using namespace std;
+
+int main() {
+
+    	std::ios::sync_with_stdio(false);
+	int testCases;
+	cin >> testCases;
+    	while(testCases--) {
+    		int n;
+    		cin >> n;
+          for(int i = 0; i < n; i++) {
+	    	    int temp;
+              cin >> temp;
+              cout << temp;
+          }
+          cout << endl;
+    	}
+    return 0;
+}
