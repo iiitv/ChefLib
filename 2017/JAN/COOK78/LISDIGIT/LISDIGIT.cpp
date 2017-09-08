1b685d0d0cfe2259f9ed6a7eb1fb7f2170db3a29
PiyushPawar17 <piyushpawar25@gmail.com>
2017-09-08 16:40:19 +0530

    Fixing codeclimate issues

--- 2017/JAN/COOK78/LISDIGIT/LISDIGIT.cpp
+++ 2017/JAN/COOK78/LISDIGIT/LISDIGIT.cpp
@@ -11,12 +11,14 @@ int main() {
     	while(testCases--) {
     		int n;
     		cin >> n;
-          for(int i = 0; i < n; i++) {
-	    	    int temp;
-              cin >> temp;
-              cout << temp;
-          }
-          cout << endl;
+    		int a[n];
+     	for(int i = 0; i < n; i++)
+	    	    cin >> a[i];
+
+	     for(int i = 0; i < n; i++)
+	    	    cout << a[i];
+
+    	     cout << endl;
     	}
     return 0;
 }
