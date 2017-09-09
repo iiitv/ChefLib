6e8aeae1470c114bd8624b9d67a045ac789e5524
Piyush Pawar <piyushpawar25@gmail.com>
2017-09-09 13:01:30 +0530

    Fixed Indentations

--- 2017/JAN/COOK78/LISDIGIT/LISDIGIT.cpp
+++ 2017/JAN/COOK78/LISDIGIT/LISDIGIT.cpp
@@ -5,18 +5,18 @@ using namespace std;
 
 int main() {
 
-    std::ios::sync_with_stdio(false);
+	std::ios::sync_with_stdio(false);
 	int testCases;
 	cin >> testCases;
-    while (testCases--) {
-          int n;
-          cin >> n;
-          int a[n];
-          for (int i = 0; i < n; i++)
-               cin >> a[i];
-          for (int i = 0; i < n; i++)
-               cout << a[i];
-          cout << endl;
-     }
-    return 0;
+	while (testCases--) {
+		int n;
+		cin >> n;
+		int a[n];
+		for (int i = 0; i < n; i++)
+			cin >> a[i];
+		for (int i = 0; i < n; i++)
+			cout << a[i];
+		cout << endl;
+	}
+	return 0;
 }
