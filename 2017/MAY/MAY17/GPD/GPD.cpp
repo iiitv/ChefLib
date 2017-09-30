b5a24337b97bdcad59f7d979d1c7f200e68b1f83
Istasis Mishra <mishra.istasis@gmail.com>
2017-10-01 00:30:59 +0530

    Update GPD.cpp

--- 2017/MAY/MAY17/GPD/GPD.cpp
+++ 2017/MAY/MAY17/GPD/GPD.cpp
@@ -13,15 +13,15 @@ struct trie
     {
         nxt[0] = nxt[1] = -1;
     }
-}ar[300002*32];//This is the trie, ar[i] is a node in the trie
+}ar[300002 * 32];//This is the trie, ar[i] is a node in the trie
 int tot = 0;
 void insert(int x, int root)
 {
     int i = 32;
     while(i--)
     {
-        int b = (bool)(x&(1<<i));
-        if(ar[root].nxt[b]==-1)
+        int b = (bool)(x & (1 << i));
+        if(ar[root].nxt[b] == -1)
         {
             ar[++tot] = trie();
             ar[root].nxt[b] = tot;
@@ -38,7 +38,6 @@ map<int, vector< pair<int,int> > > g;
 map<int, int> start;
 void dfs(pair<int,int> u, pair<int,int> p)
 {
-    //cout<<u.first<<" ";
     ar[++tot] = ar[ start[p.first] ];
     start[u.first] = tot;
     insert(u.second, start[u.first]);
@@ -57,11 +56,11 @@ int maxxor(int x, int root)
     int i = 32, ret = 0;
     while(i--)
     {
-        int b = (bool)(x&(1<<i));
-        if(ar[root].nxt[b^1]==-1)
+        int b = (bool)(x & (1 << i));
+        if(ar[root].nxt[b ^ 1]==-1)
             root = ar[root].nxt[b];
         else
-            ret+=1<<i, root = ar[root].nxt[b^1];
+            ret += 1 << i, root = ar[root].nxt[b ^ 1];
     }
     return ret;
 }
@@ -70,9 +69,9 @@ int minxor(int x, int root)
     int i = 32, ret = 0;
     while(i--)
     {
-        int b = (bool)(x&(1<<i));
+        int b = (bool)(x & (1 << i));
         if(ar[root].nxt[b]==-1)
-            ret+=1<<i, root = ar[root].nxt[b^1];
+            ret += 1 << i, root = ar[root].nxt[b ^ 1];
         else
             root = ar[root].nxt[b];
     }
@@ -88,11 +87,11 @@ int main()
     cin.tie(0);
     cout.tie(0);
     
-    cin>>n>>q>>r>>key;
+    cin >> n >> q >> r >> key;
     for(int i = 0;i<n-1;i++)
     {
         int u, v, k;
-        cin>>u>>v>>k;
+        cin >> u >> v >> k;
         g[v].push_back({u, k});
     }
     ar[0] = trie();
@@ -103,27 +102,27 @@ int main()
     while(q--)
     {
         int t;
-        cin>>t;
-        t^=la;
+        cin >> t;
+        t ^= la;
         if(t == 0)
         {
             int u, v, k;
-            cin>>v>>u>>k;
-            v^=la;
-            u^=la;
-            k^=la;
+            cin >> v >> u >> k;
+            v ^= la;
+            u ^= la;
+            k ^= la;
             addnode(v, u, k);
         }
         else
         {
             int u, k;
-            cin>>u>>k;
-            u^=la;
-            k^=la;
+            cin >> u >> k;
+            u ^= la;
+            k ^= la;
             int ans1 = maxxor(k, start[u]);
             int ans2 = minxor(k, start[u]);
-            la = ans1^ans2;
-            cout<<ans2<<" "<<ans1<<endl;
+            la = ans1 ^ ans2;
+            cout << ans2 << " " << ans1 << endl;
         }
     }
     
