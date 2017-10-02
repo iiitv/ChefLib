5e0290717b06639084488850f240bcd5b294565b
flaredragon <akashdeepramnaney@gmail.com>
2017-10-02 12:51:25 +0530

    Update LIKECS02.cpp

--- 2017/SEPT/COOK86/LIKECS02/LIKECS02.cpp
+++ 2017/SEPT/COOK86/LIKECS02/LIKECS02.cpp
@@ -5,20 +5,21 @@ using namespace std;
 int main()
 {
 	int t;
-	cin>>t;
+	cin >> t;
 	while(t--)
 	{
 		long long x;
-		cin>>x;
+		cin >> x;
 		if(x > 1)
 		{
 		long long y = x / 2;
 		long long z = y + x;
 		for(long long t = y;t < z;t++)
-		cout<<t<<" ";
-		cout<<endl;
-	}
-	else cout<<1<<endl;
+		cout << t << " ";
+		cout << endl;
+		}
+		else 
+		cout << 1 << endl;
 	}
 	
 	
