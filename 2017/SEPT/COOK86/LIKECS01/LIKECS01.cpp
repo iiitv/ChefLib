16b0983aaf4ef5b59b5c9bd727db1fe4b08e8dea
Hsankesara <heetsankesara3@gmail.com>
2017-10-02 16:15:29 +0530

    code added

---
+++ 2017/SEPT/COOK86/LIKECS01/LIKECS01.cpp
@@ -0,0 +1,28 @@
+// code written by heet
+#include <iostream>
+#include <cstring>
+
+using namespace std;
+
+int main() {
+	int t;
+	cin >> t;
+	while (t--) {
+		string s;
+		cin >> s;
+		int a[26] = {0};
+		int n = s.size();
+		bool isbreak = false;
+		for (int i = 0; i < n; ++i) {
+			a[(int) s[i] - 97]++;
+			if(a[(int) s[i] - 97 ] == 2) {
+				cout << "yes\n";
+				isbreak = true;
+				break;
+			}
+		}
+		if(!isbreak)
+			cout << "no\n";
+	}
+		return 0;
+}
