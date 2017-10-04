64a44980558cf80794841668d2f5195eb2061fcc
Abhigyan Khaund <mail@abhigyan.xyz>
2017-10-04 20:13:32 +0530

    Add spaces around equal operator

--- 2017/SEPT/SEPT17/FILLMTR/FILLMTR.py
+++ 2017/SEPT/SEPT17/FILLMTR/FILLMTR.py
@@ -24,33 +24,33 @@ def main():
             else:
                 if(good[i[0]]=='a' and good[i[1]]=='a'):
                     if(i[2]==1):
-                        good[i[0]]=1
-                        good[i[1]]=0
+                        good[i[0]] = 1
+                        good[i[1]] = 0
                     else:
-                        good[i[0]]=0
-                        good[i[1]]=0
+                        good[i[0]] = 0
+                        good[i[1]] = 0
                 elif(good[i[0]]!='a' and good[i[1]]=='a'):
                     if(good[i[0]]==1):
                         if(i[2]==1):
-                            good[i[1]]=0
+                            good[i[1]] = 0
                         else:
-                            good[i[1]]=1
+                            good[i[1]] = 1
                     else:
                         if(i[2]==1):
-                            good[i[1]]=1
+                            good[i[1]] = 1
                         else:
-                            good[i[1]]=0
+                            good[i[1]] = 0
                 elif(good[i[0]]=='a' and good[i[1]]!='a'):
                     if(good[i[1]]==1):
                         if(i[2]==1):
-                            good[i[0]]=0
+                            good[i[0]] = 0
                         else:
-                            good[i[0]]=1
+                            good[i[0]] = 1
                     else:
                         if(i[2]==1):
-                            good[i[0]]=1
+                            good[i[0]] = 1
                         else:
-                            good[i[0]]=0
+                            good[i[0]] = 0
                 else:
                     if(abs(good[i[0]] - good[i[1]]) == i[2]):
                         continue
