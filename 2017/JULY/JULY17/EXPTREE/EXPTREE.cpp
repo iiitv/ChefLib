2a7e4724ba3ea504651fa3685b142b231c515954
Nishant Tanwar <nishant.tanwar08@gmail.com>
2017-10-06 01:23:10 +0530

    Updated EXPTREE.cpp

--- 2017/JULY/JULY17/EXPTREE/EXPTREE.cpp
+++ 2017/JULY/JULY17/EXPTREE/EXPTREE.cpp
@@ -15,12 +15,10 @@ ll MOD;
 ll exponent(ll, ll);
 ll modinverse(ll);
 
-int main() {
-
+int main(){
     ll t;
     cin >> t;
-    while(t--)
-    {
+    while(t--){
         ll n;
         cin >> n;
         n--;
@@ -28,13 +26,11 @@ int main() {
         MOD = MAX1;
         x = ((n % MOD) * ((n + 1) % MOD)) % MOD;
         y = (2 * ((2 * n - 1) % MOD)) % MOD;
-        if(x % 2 == 0 && y % 2 == 0)
-        {
+        if(x % 2 == 0 && y % 2 == 0){
             x /= 2;
             y /= 2;
         }
-        if(x % 3 == 0 && y % 3 == 0)
-        {
+        if(x % 3 == 0 && y % 3 == 0){
             x /= 3;
             y /= 3;
         }
@@ -42,13 +38,11 @@ int main() {
         MOD = MAX2;
         x = ((n % MOD) * ((n + 1) % MOD)) % MOD;
         y = (2 * ((2 * n - 1) % MOD)) % MOD;
-        if(x % 2 == 0 && y % 2 == 0)
-        {
+        if(x % 2 == 0 && y % 2 == 0){
             x /= 2;
             y /= 2;
         }
-        if(x % 3 == 0 && y % 3 == 0)
-        {
+        if(x % 3 == 0 && y % 3 == 0){
             x /= 3;
             y /= 3;
         }
@@ -58,16 +52,14 @@ int main() {
     return 0;
 }
 
-ll exponent(ll a, ll b)
-{
-        if(b == 0)
-            return 1;
-        ll temp = (exponent(a, b / 2)) % MOD;
-        temp = (temp * temp) % MOD;
-        return (b % 2 == 0) ? temp : ((a % MOD) * temp) % MOD;
+ll exponent(ll a, ll b) {
+    if(b == 0)
+        return 1;
+    ll temp = (exponent(a, b / 2)) % MOD;
+    temp = (temp * temp) % MOD;
+    return (b % 2 == 0) ? temp : ((a % MOD) * temp) % MOD;
 }
 
-ll modinverse(ll val)
-{
-        return exponent(val, MOD - 2);
+ll modinverse(ll val) {
+    return exponent(val, MOD - 2);
 }
