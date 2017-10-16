9639058a363859f9f897a5aaf9f12dbb7cb204d6
PiyushPawar17 <piyushpawar25@gmail.com>
2017-10-16 11:22:27 +0530

    Fixed Issues

--- 2017/SEPT/SEPT17/CHEFPDIG/CHEFPDIG.cpp
+++ 2017/SEPT/SEPT17/CHEFPDIG/CHEFPDIG.cpp
@@ -1,3 +1,4 @@
+// Made by Piyush
 #include <bits/stdc++.h>
 #define endl "\n"
 using namespace std;
@@ -6,26 +7,24 @@ int main() {
      std::ios::sync_with_stdio(false);
      int testCases;
      cin >> testCases;
-     while(testCases--) {
-        string input;
-        vector<int> alpha(10, 0);
-        cin >> input;
-        for (int i = 0; i < input.length(); i++) {
-            //cout << "ASCII: " << input[i] - 48 << endl;
-            ++alpha[input[i] - 48];
-        }
-        for (int i = 6; i < 10; i++) {
-            if (alpha[i] > 0) {
-                for (int j = 0; j < 10; j++) {
-                    char ch = i * 10 + j;
-                    if (ch >= 65 && ch <= 90) {
-                        if (j == i && alpha[j] > 1) cout << ch;
-                        if (j != i && alpha[j] > 0) cout << ch;
+     while (testCases--) {
+          string input;
+          vector<int> alpha(10, 0);
+          cin >> input;
+          for (int i = 0; i < input.length(); i++)
+               ++alpha[input[i] - 48];
+          for (int i = 6; i < 10; i++) {
+               if (alpha[i] > 0) {
+                    for (int j = 0; i < 10; j++) {
+                         char ch = i * 10 + j;
+                         if (ch >= 65 && ch <= 90) {
+                              if (j == i && alpha[j] > 1) cout << ch;
+                              if (j != i && alpha[j] > 0) cout << ch;
+                         }
                     }
-                }
-            }
-        }
-        cout << endl;
+               }
+          }
+          cout << endl;
      }
      return 0;
 }
