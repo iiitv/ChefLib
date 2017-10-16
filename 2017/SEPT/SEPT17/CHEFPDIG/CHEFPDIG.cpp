95486c601c258b78a2adf53b04f8846015fbc324
PiyushPawar17 <piyushpawar25@gmail.com>
2017-10-16 11:09:05 +0530

    Added CHEFPDIG in cpp

---
+++ 2017/SEPT/SEPT17/CHEFPDIG/CHEFPDIG.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+#define endl "\n"
+using namespace std;
+
+int main() {
+    std::ios::sync_with_stdio(false);
+    int testCases;
+    cin >> testCases;
+    while(testCases--) {
+        string input;
+        vector<int> alpha(10, 0);
+        cin >> input;
+        for (int i = 0; i < input.length(); i++) {
+            //cout << "ASCII: " << input[i] - 48 << endl;
+            ++alpha[input[i] - 48];
+        }
+        for (int i = 6; i < 10; i++) {
+            if (alpha[i] > 0) {
+                for (int j = 0; j < 10; j++) {
+                    char ch = i * 10 + j;
+                    if (ch >= 65 && ch <= 90) {
+                        if (j == i && alpha[j] > 1) cout << ch;
+                        if (j != i && alpha[j] > 0) cout << ch;
+                    }
+                }
+            }
+        }
+        cout << endl;
+    }
+    return 0;
+}
