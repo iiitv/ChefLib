b790811dbd414cf5ae8b59c85284161dc7b64c70
PiyushPawar17 <piyushpawar25@gmail.com>
2017-10-16 11:29:26 +0530

    Fixed Issues

--- 2017/SEPT/SEPT17/CHEFPDIG/CHEFPDIG.cpp
+++ 2017/SEPT/SEPT17/CHEFPDIG/CHEFPDIG.cpp
@@ -4,27 +4,27 @@
 using namespace std;
 
 int main() {
-     std::ios::sync_with_stdio(false);
-     int testCases;
-     cin >> testCases;
-     while (testCases--) {
-          string input;
-          vector<int> alpha(10, 0);
-          cin >> input;
-          for (int i = 0; i < input.length(); i++)
-               ++alpha[input[i] - 48];
-          for (int i = 6; i < 10; i++) {
-               if (alpha[i] > 0) {
-                    for (int j = 0; i < 10; j++) {
-                         char ch = i * 10 + j;
-                         if (ch >= 65 && ch <= 90) {
-                              if (j == i && alpha[j] > 1) cout << ch;
-                              if (j != i && alpha[j] > 0) cout << ch;
-                         }
-                    }
-               }
-          }
-          cout << endl;
-     }
-     return 0;
+	std::ios::sync_with_stdio(false);
+	int testCases;
+	cin >> testCases;
+	while (testCases--) {
+		string input;
+		vector<int> alpha(10, 0);
+		cin >> input;
+		for (int i = 0; i < input.length(); i++)
+			++alpha[input[i] - 48];
+		for (int i = 6; i < 10; i++) {
+			if (alpha[i] > 0) {
+				for (int j = 0; i < 10; j++) {
+					char ch = i * 10 + j;
+					if (ch >= 65 && ch <= 90) {
+						if (j == i && alpha[j] > 1) cout << ch;
+						if (j != i && alpha[j] > 0) cout << ch;
+					}
+				}
+			}
+		}
+		cout << endl;
+	}
+	return 0;	
 }
