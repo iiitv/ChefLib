079203d5bfb01aa9b28adbe5ebd29ad8b0830fe3
Lucifer24 <divyeshpuri66@gmail.com>
2017-10-24 20:14:45 +0530

    Added MINPERM

---
+++ 2017/SEPT/SEPT17/MINPERM/MINPERM.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+int main()	{
+	int t;
+	scanf("%d", &t);
+	while(t--) {
+		int n;
+		scanf("%d", &n);
+		int arr[n + 1];
+		int i, j, k, l;
+		for(i = 1; i <= n; i++) {
+			arr[i] = i;
+		}
+		if (n % 2 == 0)	{
+			for(i = 1; i < n; i += 2) {
+				int temp = arr[i];
+				arr[i] = arr[i + 1];
+				arr[i + 1] = temp;
+			}
+		}
+		else {
+			for(i = 1; i < n; i += 2) {
+				int temp = arr[i];
+				arr[i] = arr[i + 1];
+				arr[i + 1] = temp;
+			}
+			j = arr[n - 1];
+			arr[n - 1] = arr[n];
+			arr[n] = j;	
+		}
+		for(i = 1; i <= n; i++) {
+			printf("%d ",arr[i]);
+		}
+		printf("\n");
+	}
+}
