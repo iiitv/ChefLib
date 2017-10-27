049da6395c72942efb428d3e4f2d2c3ff732e791
Lucifer24 <divyeshpuri66@gmail.com>
2017-10-27 10:50:42 +0530

    Issues Fixed

--- 2017/SEPT/SEPT17/CHEFSUM/CHEFSUM.c
+++ 2017/SEPT/SEPT17/CHEFSUM/CHEFSUM.c
@@ -1,18 +1,19 @@
+//Made by Divyesh Puri
 #include <stdio.h>
-int main()	{
+int main(){
 	int t;
 	scanf("%d", &t);
-	while (t--) {
+	while (t--){
 		int n, i, k = 100000, index;
 		scanf("%d", &n);
 		int arr[n];
-		for (i = 0; i < n; i++)	{
+		for (i = 0; i < n; i++){
 			scanf("%d", &arr[i]);
-			if (arr[i] < k)	{
+			if (arr[i] < k){
 				k = arr[i];
 				index = i;
 			}
 		}
 		printf("%d\n",index + 1);
-	}		
+	}
 }
